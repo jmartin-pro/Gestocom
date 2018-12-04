98f14f6a290800ecbab66a084a163c837ea7a589
cvincent <corentino.vincent@gmail.com>
2018-12-04 14:53:22 +0100

    correction push

--- Gestocom/src/Entity/Habitation.php
+++ Gestocom/src/Entity/Habitation.php
@@ -2,11 +2,9 @@
 
 namespace App\Entity;
 
-<<<<<<< HEAD
-=======
+
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
->>>>>>> master
 use Doctrine\ORM\Mapping as ORM;
 
 /**
