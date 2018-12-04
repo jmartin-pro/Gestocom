67c78d520b4566fd55dd19540d3961af1252a48d
cvincent <corentino.vincent@gmail.com>
2018-12-04 16:42:11 +0100

    Ajout de la fonctionniolité "Ajouter une habitation"

---
+++ Gestocom/src/Form/HabitationType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Habitation;
+use App\Entity\Usager;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeInterface;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class HabitationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('adresse')
+            ->add('copos')
+            ->add('ville')
+            ->add('usager',EntityType::class,array(
+                'class' => Usager::class,
+                'choice_label'=>'mail'
+  
+              ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Habitation::class,
+        ]);
+    }
+}
