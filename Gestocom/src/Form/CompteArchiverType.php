f0e0647e5c28431961c6e054f465918f1cd5ebb2
aurianeA <auriane.assemat@gmail.com>
2018-12-07 10:48:50 +0100

    Ajout de la fonctionnalité en tant que resposable je peux gérer les comptes

---
+++ Gestocom/src/Form/CompteArchiverType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Compte;
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class CompteArchiverType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login', TextType::class, array('disabled'=> true))
+            ->add('mdp', TextType::class, array('disabled'=> true))
+			->add('enregistrer', SubmitType::class, array('label' => 'Archiver compte'))
+        ;
+    }
+	
+	
+	public function getParent(){
+      return CompteType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Compte::class,
+        ]);
+    }
+}
