9d81e6234f6e6f6f7da5df27206b9832c66b6f3f
cvincent <corentino.vincent@gmail.com>
2018-12-18 11:11:02 +0100

    Reconstruction de la page d'accueil

--- Gestocom/public/js/bootstrap.js
+++ Gestocom/public/js/bootstrap.js
@@ -22,6 +22,43 @@
     }
   }
 
+  $(document).ready(function(){
+    var $window = $(window);
+      $('section[data-type="background"]').each(function(){
+          var $bgobj = $(this); // assigning the object
+      
+          $(window).scroll(function() {
+              var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+              
+              // Put together our final background position
+              var coords = '50% '+ yPos + 'px';
+  
+              // Move the background
+              $bgobj.css({ backgroundPosition: coords });
+          }); 
+      });    
+  });
+  
+  //menu transition js
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+          if (scroll > 0) {
+        $(".navbar").addClass("navbar-scroll");
+          }
+          else{
+        $(".navbar").removeClass("navbar-scroll");  	
+      }
+      if (scroll > 200) {
+        $(".navbar").addClass("bg-primary");
+      }
+  
+      else{
+        $(".navbar").removeClass("bg-primary");  	
+      }
+    })
+  })
+  
   function _createClass(Constructor, protoProps, staticProps) {
     if (protoProps) _defineProperties(Constructor.prototype, protoProps);
     if (staticProps) _defineProperties(Constructor, staticProps);
